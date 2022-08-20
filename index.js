72022ea7d3fa40f3bd46d11aaba2ac9bd1cbcc23
flake <davekarry1337@gmail.com>
2022-08-20 12:40:37 +0300

    feat(eslint): базовая настройка линтера

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const app = express()
-require('dotenv').config();
-require('dotenv-defaults').config()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+require("dotenv-defaults").config();
 
 
 
 async function main() {
-    await app.listen(Number(process.env.APP_PORT));
-    console.log(`Сервер открыт на http://localhost:${process.env.APP_PORT}/`);  
+	await app.listen(Number(process.env.APP_PORT));
+	console.log(`Сервер открыт на http://localhost:${process.env.APP_PORT}/`);  
 }
 
 main().catch(err => console.error(err));
