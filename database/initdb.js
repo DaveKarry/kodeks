ca8540201013682707d169f442e051d0ca063f47
flake <davekarry1337@gmail.com>
2022-08-20 14:06:59 +0300

    feat(dbSeq): Подключена бд

---
+++ database/initdb.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+require('dotenv-defaults').config();
+
+const port = process.env.DB_PORT;
+const db_name = process.env.DB_NAME;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_host = process.env.DB_HOST;
+
+const sequelize = new Sequelize(`postgres://${db_user}:${db_pass}@${db_host}:${port}/${db_name}`);
+
+
+module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sequelize = require('./database/initdb');
 const app = express();
 require('dotenv').config();
 require('dotenv-defaults').config();
@@ -7,6 +8,12 @@ require('dotenv-defaults').config();
 async function main() {
   await app.listen(Number(process.env.APP_PORT));
   console.log(`Сервер открыт на http://localhost:${process.env.APP_PORT}/`);
+  try {
+    await sequelize.authenticate();
+    console.log('Соединение с БД открыто...');
+  } catch (error) {
+    console.error('Непредвиденная ошибка:', error);
+  }
 }
 
 main().catch(err => console.error(err));
