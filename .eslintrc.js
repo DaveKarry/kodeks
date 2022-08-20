0f6aaacfe6502fbe351cdde18608f224d50c8fcf
flake <davekarry1337@gmail.com>
2022-08-20 13:00:02 +0300

    fix(eslint): Добавочные правила линтера

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 0 }],
+    'no-trailing-spaces': ['error', { 'skipBlankLines': true }]
+  }
+};
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-require("dotenv").config();
-require("dotenv-defaults").config();
-
+require('dotenv').config();
+require('dotenv-defaults').config();
 
 
 async function main() {
-	await app.listen(Number(process.env.APP_PORT));
-	console.log(`Сервер открыт на http://localhost:${process.env.APP_PORT}/`);  
+  await app.listen(Number(process.env.APP_PORT));
+  console.log(`Сервер открыт на http://localhost:${process.env.APP_PORT}/`);
 }
 
 main().catch(err => console.error(err));
