e3941e63d0c0f7d12998ada15fdfb0829873f103
flake <davekarry1337@gmail.com>
2022-08-21 15:34:04 +0300

    fix(route): Изменены роуты по принципу /api/<tag>/<version>/endpoint

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -2,8 +2,8 @@ const Router = require('express');
 const userController = require('../controllers/userController');
 const router = new Router();
 
-router.post('/registration', userController.registration);
-router.post('/login', userController.login);
+router.post('/v1/registration', userController.registration);
+router.post('/v1/login', userController.login);
 
 
 module.exports = router;
