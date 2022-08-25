38ecc3caba4a42538d620ab19ee83554563bc873
Kodintsev K <DaveKarry1337@gmail.com>
2022-08-25 15:14:03 +0300

    fix(logger): Теперь показывается метод, которым был отправлен запрос

--- controllers/logger.js
+++ controllers/logger.js
@@ -25,16 +25,17 @@ const logger = createLogger({
 });
 
 function logError(data) {
-  logger.log('error', `login - ${data.login}, ip - ${data.ip}, request - ${data.request}, status - ${data.status}, message - ${data.message}`);
+  logger.log('error', `login - ${data.login}, ip - ${data.ip}, request - ${data.method}: ${data.request}, status - ${data.status}, message - ${data.message}`);
 }
 
 function logSuccess(data) {
-  logger.log('info', `login - ${data.login}, ip - ${data.ip}, request - ${data.request}, status - 200`);
+  logger.log('info', `login - ${data.login}, ip - ${data.ip}, request - ${data.method}: ${data.request}, status - 200`);
 }
 
 function createDatalog(req){
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const token = req?.headers?.authorization?.split(' ')[1];
+  const {method} = req;
   let decode;
   if (token){
     decode = jwt.verify(token, process.env.SECRET_KEY);
@@ -44,6 +45,7 @@ function createDatalog(req){
   return {
     ip,
     login,
+    method,
     request: req.originalUrl
   };
 }
