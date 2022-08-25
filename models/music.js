023181bd605df5643e0df05ec473769c2d943a6d
Kodintsev K <DaveKarry1337@gmail.com>
2022-08-25 16:42:01 +0300

    feat(music): Модель музыки

---
+++ models/music.js
@@ -0,0 +1,27 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+
+module.exports = (sequelize) => {
+
+  return sequelize.define('music', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        isLowercase: true,
+      }
+    },
+    path: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+      }
+    }
+  });
+};
